14546cba26b281680745cbc95a3d2599438f4e59
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-01-10 16:14:14 +0900

    [D1] Title: 최대수 구하기, Time: 154 ms, Memory: 57,468 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.py
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.py
@@ -2,7 +2,9 @@ T = int(input())
 
 for t in range(1,T+1):
     numbers = list(map(int,input().split()))
-
-    a = max(numbers)
+    a = 0
+    for i in numbers:
+        if i > a:
+            a = i
 
     print(f"#{t} {a}")
