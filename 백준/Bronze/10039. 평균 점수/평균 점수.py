ce69d05d4b8124637b1883e3a112e39d02ca0306
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-01-16 14:24:00 +0900

    [Bronze IV] Title: 평균 점수, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10039. 평균 점수/평균 점수.py
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -1,11 +1,18 @@
-average = 0 
-for i in range(5):
-    num = int(input())
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+num5 = int(input())
 
-    if num < 40:
-        num =40
+num = [num1,num2,num3,num4,num5]
+num_list = []
+for i in num:
+    if i < 40:
+        i = 40
+        num_list.append(i)
+    else:
+        num_list.append(i)
 
+a = int(sum(num_list)/5)
 
-    average += num
-
-print(average//5)
+print(a)
