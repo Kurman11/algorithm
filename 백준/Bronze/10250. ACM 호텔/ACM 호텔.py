d41cc7fdaa7f2efbce18f0017b232833d4a04d22
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-02-16 23:17:41 +0900

    [Bronze III] Title: ACM 호텔, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,15 @@
+T = int(input())
+
+for t in range(T):
+    H, W, N = map(int,input().split())
+
+    cnt = 1
+    
+    while 1:
+        if N > H:
+            N = N - H
+            cnt += 1
+        else:
+            cnt_str = str(cnt)
+            print(f'{N}{cnt_str.zfill(2)}')
+            break
