b2dde88419c4ede7e0799eec594641eeb250130b
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-02-22 19:45:51 +0900

    [Silver IV] Title: 카드2, Time: 244 ms, Memory: 51020 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,14 @@
+from collections import deque
+T = int(input())
+card = deque()
+for i in range(1,T+1):
+    card.append(i)
+if len(card) != 1:
+    while 1:
+        card.popleft()
+        if len(card) == 1:
+            break
+        card.append(card.popleft())  
+        if len(card) == 1:
+            break
+print(*card)
