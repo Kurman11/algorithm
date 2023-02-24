c05930fb030efca53496be66b7369e1fb662b320
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-02-24 22:55:32 +0900

    [Silver III] Title: 카드 놓기, Time: 832 ms, Memory: 86192 KB -BaekjoonHub

---
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+T = int(input())
+num = list(map(int,input().split()))
+b = deque()
+for x in range(1,T+1):
+    a = num.pop()
+    if a == 1:
+        b.appendleft(x)
+    elif a == 2:
+        b.insert(1, x)
+    elif a == 3:
+        b.append(x)
+print(*b)
