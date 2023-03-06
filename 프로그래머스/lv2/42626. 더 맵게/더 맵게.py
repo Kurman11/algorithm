c81e2e815c96354df43b01c3142b0b3ac2d8e1e8
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-03-06 20:18:37 +0900

    [level 2] Title: 더 맵게, Time: 2109.43 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,54 +1,54 @@
-# import heapq
-
-# lst = []
-# cnt =0
-# a = []
-# def solution(scoville, K):
-#     global cnt
-#     for i in scoville:
-#         heapq.heappush(lst,i)
-    
-#     while 1:
-#         first = heapq.heappop(lst)
-#         if first >= K:
-#             break
-#         second = heapq.heappop(lst)
-#         mix = first + second * 2
-        
-#         if mix < K:
-#             cnt += 1
-#             heapq.heappush(lst,mix)
-#         else:
-#             a.append(mix)
-#             cnt += 1
-        
-#         if len(lst) == 0:
-#             break            
-#         elif cnt == len(scoville)-1:
-#             cnt = -1
-#             break
-    
-#     return cnt
-    
 import heapq
 
+lst = []
+cnt = 0
 def solution(scoville, K):
-    lst = []
-    cnt = 0
-
+    global cnt
     for i in scoville:
-        heapq.heappush(lst, i)
-
-    while len(lst) > 1:
+        heapq.heappush(lst,i)
+    
+    while 1:
+        if len(scoville) == 1:
+            break
+        
         first = heapq.heappop(lst)
         if first >= K:
             break
         second = heapq.heappop(lst)
         mix = first + second * 2
-        heapq.heappush(lst, mix)
+        heapq.heappush(lst,mix)
         cnt += 1
-
-    if lst[0] >= K:
+        
+        if len(lst) == 0:
+            break            
+        elif cnt == len(scoville)-1:
+            break
+        
+    if lst[0] > K:
         return cnt
     else:
         return -1
+            
+    
+# import heapq
+
+# def solution(scoville, K):
+#     lst = []
+#     cnt = 0
+
+#     for i in scoville:
+#         heapq.heappush(lst, i)
+
+#     while len(lst) > 1:
+#         first = heapq.heappop(lst)
+#         if first >= K:
+#             break
+#         second = heapq.heappop(lst)
+#         mix = first + second * 2
+#         heapq.heappush(lst, mix)
+#         cnt += 1
+
+#     if lst[0] >= K:
+#         return cnt
+#     else:
+#         return -1
