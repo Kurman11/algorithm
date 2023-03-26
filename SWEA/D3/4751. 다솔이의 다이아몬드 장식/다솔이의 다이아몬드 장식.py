3cb708860b73767d910082aef5ff7c7abf99cc60
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-03-26 11:16:34 +0900

    [D3] Title: 다솔이의 다이아몬드 장식, Time: 244 ms, Memory: 64,256 KB -BaekjoonHub

--- SWEA/D3/4751. 다솔이의 다이아몬드 장식/다솔이의 다이아몬드 장식.py
+++ SWEA/D3/4751. 다솔이의 다이아몬드 장식/다솔이의 다이아몬드 장식.py
@@ -4,30 +4,26 @@ for t in range(1,T+1):
     word = input()
     n = len(word) 
 
-    if n == 1:
-        print('..#..''\n''.#.#.''\n'f'#.{word}.#''\n''.#.#.''\n''..#..')
-    elif n >= 2:
-        for i in range(n):
-            print("..#.", end="")
-            if i == n-1:
-                print(".")
-
-        for i in range(n):
-            print('.#.#',end='')
-            if i == n-1:
-                print('.')
-        
-        for i in range(n):
-            print('#.'f'{word[i]}.',end='')
-            if i == n-1:
-                print('#')
-        
-        for i in range(n):
-            print('.#.#',end='')
-            if i == n-1:
-                print('.')
-
-        for i in range(n):
-            print("..#.", end="")
-            if i == n-1:
-                print(".")
+    for i in range(n):
+        print("..#.", end="")
+        if i == n-1:
+            print(".")
+    for i in range(n):
+        print('.#.#',end='')
+        if i == n-1:
+            print('.')
+    
+    for i in range(n):
+        print('#.'f'{word[i]}.',end='')
+        if i == n-1:
+            print('#')
+    
+    for i in range(n):
+        print('.#.#',end='')
+        if i == n-1:
+            print('.')
+            
+    for i in range(n):
+        print("..#.", end="")
+        if i == n-1:
+            print(".")
