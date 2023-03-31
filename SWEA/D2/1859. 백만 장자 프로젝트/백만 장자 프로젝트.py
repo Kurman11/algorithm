624be6db7c1733fae383e03796a3e3e3c7807d13
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-03-31 09:12:00 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,190 ms, Memory: 238,420 KB -BaekjoonHub

---
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -0,0 +1,21 @@
+
+T = int(input())
+
+for t in range(1,T+1):
+    a = []
+    num = int(input())
+    Hoarding = list(map(int,input().split()))
+    cnt = 0
+    max_num = 0
+    while 1:
+        if Hoarding[-1] > max_num:
+            max_num = Hoarding[-1]
+            Hoarding.pop()
+        else:
+            cnt += max_num - Hoarding[-1]
+            Hoarding.pop()
+
+        if len(Hoarding) == 0:
+            break
+    
+    print(f'#{t} {cnt}')
