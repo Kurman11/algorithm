aa870b59c63ee3e6e46305098f9359a4686eb7a9
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-04-05 19:55:00 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,19 +1,18 @@
-def dfs(n, computers, start, visited):
-    visited[start] = True
-    
-    for i in range(0,n):
-        if (visited[i] == False and computers[start][i] == 1):
-            visited = dfs(n, computers, i, visited)
-    return visited
-
-
 def solution(n, computers):
     visited = [False]*n
     answer = 0
-    
-    for start in range(0,n):
-        if (visited[start] == False):
-            dfs(n, computers, start, visited)
+    for i in range(0,n):
+        if visited[i] == False:
+            dfs(n,computers, i, visited)
             answer += 1
-    
+            
     return answer
+
+
+def dfs(n, computers, i, visited):
+    visited[i] = True
+    
+    for x in range(0,n):
+        if visited[x] == False and computers[i][x] == 1:
+           dfs(n, computers, x, visited) 
+    return visited
