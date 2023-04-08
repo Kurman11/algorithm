02459c891e17bb50279997df92c75ca586cc37a2
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-04-08 10:28:24 +0900

    [Bronze I] Title: 소인수분해, Time: 828 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,12 @@
+def factorization(x):
+    d = 2
+
+    while d <= x:
+        if x % d == 0:
+            print(d)
+
+            x = x/d
+        else:
+            d = d +1
+
+factorization(int(input()))
