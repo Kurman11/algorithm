f9cf37574e9e8f195bb2ff4d06cce6c65c55086e
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-04-09 15:47:46 +0900

    [Silver II] Title: 유기농 배추, Time: 312 ms, Memory: 31620 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,39 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+dx = [0,0,-1,1]
+dy = [1,-1, 0,0]
 
-t = int(input())
+def dfs(x,y):
+    global visited
+    visited[x][y] = 1
+    for i in range(4):
+        newX = x + dx[i]
+        newY = y + dy[i]
+        if 0 <= newX < N and 0 <= newY< M:
+            if visited[newX][newY] == 0 and graph[newX][newY] == 1:
+                dfs(newX, newY)
+        # if graph[newX][newY] and not visited[newX][newY]:
+        #     dfs(newX,newY)
 
-def bfs(graph, a, b):
-    queue = deque()
-    queue.append((a,b))
-    graph[a][b] = 0
 
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx < 0 or nx >=n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
-    return
 
-for i in range(t):
-    cnt = 0
-    n, m, k = map(int,input().split())
-    graph = [[0]*m for _ in range(n)]
+T = int(input())
+for _ in range(T):
+    M,N,K = map(int,input().split())
+    graph = [[0] * M for _ in range(N)]
+    visited = [[0] * M for _ in range(N)]
+    for _ in range(K):
+        a, b =map(int,input().split())
+        graph[b][a] = 1
 
-    for j in range(k):
-        x, y = map(int, input().split())
-        graph[x][y] = 1
 
-    for a in range(n):
-        for b in range(m):
-            if graph[a][b] == 1:
-                bfs(graph, a, b)
-                cnt += 1
-    print(cnt)
+    answer = 0
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] and  not visited[i][j]:
+                dfs(i,j)
+                answer += 1
+
+
+    print(answer)
