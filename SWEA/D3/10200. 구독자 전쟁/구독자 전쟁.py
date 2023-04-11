fae0f826b8f79e2cc9211f958b94a0edbe86899b
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-04-11 21:21:24 +0900

    [D3] Title: 구독자 전쟁, Time: 332 ms, Memory: 62,508 KB -BaekjoonHub

---
+++ SWEA/D3/10200. 구독자 전쟁/구독자 전쟁.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for t in range(1,T+1):
+    N,A,B = map(int,input().split())
+    min_num = min(A,B)
+    if N > A+B:
+        num = 0
+    else:
+        num = A+B-N
+    
+    print(f'#{t} {min_num} {num}')
