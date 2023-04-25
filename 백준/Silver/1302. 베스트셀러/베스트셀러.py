625f82289dbf4213a3d8d218343311a2a7a332d3
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-04-25 22:55:35 +0900

    [Silver IV] Title: 베스트셀러, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+dick = {}
+
+for _ in range(T):
+    word = input()
+    if word not in dick:
+        dick[word] = 1
+    else:
+        dick[word] = dick[word] + 1
+    
+a = []
+
+for key,value in dick.items():
+    if max(dick.values()) == value:
+        a.append(key)
+
+b = sorted(a)
+print(b[0])
