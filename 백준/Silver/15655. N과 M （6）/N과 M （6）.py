9d3b47c76bdd7401d41bbda45394f3ec269c296e
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-08-03 21:09:51 +0900

    [Silver III] Title: N과 M (6), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,21 @@
+def dfs(n,rst,last_num):
+    if M == n:
+        ans.append(rst)
+        return
+    for i in range(N):
+        if v[i] == 0 and i > last_num:
+            v[i] = 1
+            dfs(n+1 , rst+[lst[i]],i)
+            v[i] = 0
+
+N, M = map(int,input().split())
+lst = sorted(list(map(int,input().split())))
+v = [0] * N
+v_1 = [0] * N
+
+ans = []
+
+dfs(0,[],-1)
+
+for i in ans:
+    print(*i)
