74dd6dac505d76edd4c3efcc9b52fdf4cdb1b3d7
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-08-06 17:53:26 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,18 +1,34 @@
-def solution(n, computers):
-    visited = [False]*n
-    answer = 0
-    for i in range(0,n):
-        if visited[i] == False:
-            dfs(n,computers, i, visited)
-            answer += 1
+def dfs(x, computers, visited):
+    visited[x] = True
+    for a,b in enumerate(computers[x]):
+        if b == 1 and (not visited[a]):
+            dfs(a, computers, visited)
             
-    return answer
-
-
-def dfs(n, computers, i, visited):
-    visited[i] = True
+def solution(n, computers):
+    visited = [False] * n
+    cnt = 0
     
-    for x in range(0,n):
-        if visited[x] == False and computers[i][x] == 1:
-           dfs(n, computers, x, visited) 
-    return visited
+    for i in range(n):
+        if not visited[i]:
+            dfs(i,computers, visited)
+            cnt += 1
+    return cnt
+
+# def solution(n, computers):
+#     visited = [False]*n
+#     answer = 0
+#     for i in range(0,n):
+#         if visited[i] == False:
+#             dfs(n,computers, i, visited)
+#             answer += 1
+            
+#     return answer
+
+
+# def dfs(n, computers, i, visited):
+#     visited[i] = True
+    
+#     for x in range(0,n):
+#         if visited[x] == False and computers[i][x] == True:
+#            dfs(n, computers, x, visited) 
+#     return visited
