3a6ebadfca2b6be6262a9d8f1864a60a9e3c2e7c
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-08-22 09:17:45 +0900

    [Gold III] Title: 치즈, Time: 592 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -2,35 +2,44 @@ from collections import deque
 
 N, M = map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(N)]
-dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
 cnt = 0
 
-while True:
+def bfs():
     q = deque()
-    v = [[0 for _ in range(M)] for _ in range(N)]
-    v[0][0] = 1
-    q.append([0, 0])
+    q.append((0,0))
+    v[0][0] == 1
+
     while q:
-        x, y = q.popleft()
+        ci, cj = q.popleft()
         for di, dj in ((-1,0), (1,0), (0,1), (0,-1)):
-            nx, ny = x + di, y + dj
-            if 0 <= nx < N and 0 <= ny < M and not v[nx][ny]:
-                if arr[nx][ny]:
-                    arr[nx][ny] += 1
-                else:
-                    v[nx][ny] = 1
-                    q.append([nx, ny])
-    
-    flag = 0
+            ni , nj = ci + di, cj + dj
+            if 0 <= ni < N and 0 <= nj < M :
+                if v[ni][nj] == 0 and arr[ni][nj] == 0:
+                    q.append((ni,nj))
+                    v[ni][nj] = 1
+                elif arr[ni][nj] == 1:
+                    v[ni][nj] += 1
+
+
+while 1:
+    v = [[0] * M for _ in range(N)] 
+    bfs()
+    cnt += 1
+    # cnt_1 = 0
     for i in range(N):
         for j in range(M):
-            if arr[i][j] >=3:
+            if v[i][j] >= 2:
                 arr[i][j] = 0
-            elif 0 < arr[i][j]:
-                flag = 1
-                arr[i][j] = 1
-    cnt +=1
-
-    if not flag:
-        print(cnt)
+    #         elif arr[i][j] == 0:
+    #             cnt_1 += 1
+    # if cnt_1 == (N*M):
+    #     break
+    cnt_1 = 0
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 0:
+                cnt_1 += 1
+    if cnt_1 ==(N*M):
         break
+
+print(cnt)
