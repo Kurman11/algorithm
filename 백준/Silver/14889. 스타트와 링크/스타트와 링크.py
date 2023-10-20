f7ab54fb884fc852255793f858d8b694e6a5158c
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-10-20 17:20:26 +0900

    [Silver I] Title: 스타트와 링크, Time: 4524 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,27 @@
+def cal(alst,blst):
+    asm = bsm = 0
+    for i in range(M):
+        for j in range(M):
+            asm += arr[alst[i]][alst[j]]
+            bsm += arr[blst[i]][blst[j]]
+    return abs(asm - bsm)
+            
+def dfs(n, alst, blst):
+    global ans
+    if n == N:
+        if len(alst) == len(blst):
+            ans = min(ans, cal(alst,blst))
+        return
+    
+    dfs(n+1, alst+[n], blst)
+    dfs(n+1, alst, blst+[n])
+
+
+N = int(input())
+arr = [list(map(int,input().split())) for _ in range(N)]
+
+M = N // 2
+ans = 100*M*M
+
+dfs(0, [], [])
+print(ans)
