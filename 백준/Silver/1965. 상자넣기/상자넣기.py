75e0dadae7a06f1d056692c019df4f0442f61c2b
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-10-30 11:42:11 +0900

    [Silver II] Title: 상자넣기, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,14 @@
+n = int(input())
+lst = list(map(int, input().split()))
+arr = [0] * n
+
+for i in range(len(lst)):
+    tmp = []
+    for j in range(i):
+        if lst[j] < lst[i]:
+            tmp.append(arr[j])
+    
+    if tmp:
+        arr[i] += max(tmp) + 1
+
+print(max(arr) + 1)
