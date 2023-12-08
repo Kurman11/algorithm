5fde29091e0aaf42e96cac3bc6764ea5656fc6a1
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-12-08 23:11:06 +0900

    [level 2] Title: 숫자의 표현, Time: 3.43 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,14 @@
+def solution(n) :
+    answer = 0
+
+    for i in range(1, n // 2 + 1) :
+        sum_value = 0
+        for j in range(i, n + 1) :
+            sum_value += j
+            if sum_value == n :
+                answer += 1
+                break
+            elif sum_value > n :
+                break
+
+    return answer + 1
