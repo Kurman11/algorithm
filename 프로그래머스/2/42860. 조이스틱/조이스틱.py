ae7dc36c2086fd000daa4b0ad30da2852bf23134
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2023-12-12 11:38:46 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -5,7 +5,6 @@ def solution(name):
     for i in range(len(name)):
         answer += min(ord(name[i]) - ord('A') , ord('Z') - ord(name[i]) + 1)
         next_d = i + 1
-        up_d = 0
         while next_d < len(name) and name[next_d] == 'A':
             next_d += 1
         min_move = min(min_move, i + i + len(name) - next_d)
