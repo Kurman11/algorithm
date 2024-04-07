a0712bba1ddc2ec93d02a73fbdc8d8d01c5aa910
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2024-04-07 22:31:30 +0900

    [level 2] Title: 땅따먹기, Time: 153.24 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -0,0 +1,8 @@
+def solution(land):
+    n = len(land)
+    for i in range(0, n-1):
+        land[i+1][0] += max(land[i][1],land[i][2],land[i][3])
+        land[i+1][1] += max(land[i][0],land[i][2],land[i][3])  
+        land[i+1][2] += max(land[i][0],land[i][1],land[i][3])
+        land[i+1][3] += max(land[i][0],land[i][1],land[i][2]) 
+    return max(land[n-1])
