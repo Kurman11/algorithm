5b227352bc4a8ea6400aacd0ccae92c667c0dd57
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2024-04-20 23:03:55 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,9 @@
+def solution(ingredient):
+    s = []
+    cnt = 0
+    for i in ingredient:
+        s.append(i)
+        if s[-4:] == [1, 2, 3, 1]:
+            cnt += 1
+            del s[-4:]
+    return cnt
