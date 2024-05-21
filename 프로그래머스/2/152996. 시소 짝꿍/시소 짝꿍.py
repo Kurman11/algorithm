fa9873e5556a89c2aabd5b51803038554785acaa
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2024-05-21 23:15:30 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+
+def solution(weights):
+    answer = 0
+    
+    # 1:1
+    counter = Counter(weights)
+    for k,v in counter.items():
+        if v>=2:
+            answer+= v*(v-1)//2
+    weights = set(weights) 
+    
+    # 2:3 2:4 3:4 비율 가지면 짝궁 가능함
+    for w in weights:
+        if w*2/3 in weights:
+            answer+= counter[w*2/3] * counter[w]
+        if w*2/4 in weights:
+            answer+= counter[w*2/4] * counter[w]
+        if w*3/4 in weights:
+            answer+= counter[w*3/4] * counter[w]
+    return answer
