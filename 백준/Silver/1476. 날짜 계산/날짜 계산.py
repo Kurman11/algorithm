360f43861fc98e6f137da11d8e203abb2005d9d0
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2024-06-13 11:38:10 +0900

    [Silver V] Title: 날짜 계산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.py
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -1,27 +1,12 @@
-E, S, M = map(int,input().split())
+E, S, M = map(int, input().split())
 
 cnt = 1
-Earth = 1
-Sun = 1
-Moon = 1
+Earth, Sun, Moon = 1, 1, 1
 
-while 1:
-    if (Earth, Sun, Moon) ==  (E, S, M):
-        break
+while (Earth, Sun, Moon) != (E, S, M):
+    Earth = (Earth % 15) + 1
+    Sun = (Sun % 28) + 1
+    Moon = (Moon % 19) + 1
+    cnt += 1
 
-    if 1 <= Earth <= 15 and 1 <= Sun <= 28 and 1 <= Moon <= 19:
-        Earth += 1
-        Sun += 1
-        Moon += 1
-        cnt += 1
-
-    elif Earth > 15:
-        Earth = 1
-        
-    elif Sun > 28:
-        Sun = 1
-
-    elif Moon > 19:
-        Moon = 1
-    
 print(cnt)
