b8b2f8dde590359932fceadb52bfb14ae60fdb48
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2024-06-27 22:19:42 +0900

    [Silver I] Title: 스타트와 링크, Time: 3400 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,27 +1,25 @@
-def cal(alst,blst):
-    asm = bsm = 0
-    for i in range(M):
-        for j in range(M):
-            asm += arr[alst[i]][alst[j]]
-            bsm += arr[blst[i]][blst[j]]
-    return abs(asm - bsm)
-            
-def dfs(n, alst, blst):
-    global ans
-    if n == N:
-        if len(alst) == len(blst):
-            ans = min(ans, cal(alst,blst))
-        return
-    
-    dfs(n+1, alst+[n], blst)
-    dfs(n+1, alst, blst+[n])
+def dfs(n,sm):
+    global result
+    if n == T//2:
+        start, link = 0,0
+        for i in range(T):
+            for j in range(i+1,T):
+                if visited[i] and visited[j]:
+                    start += (arr[i][j] + arr[j][i])
+                elif not visited[i] and not visited[j]:
+                    link += (arr[i][j] + arr[j][i])
+        
+        result = min(result, abs(start-link))
 
+    for i in range(sm, T):
+        if not visited[i]:
+            visited[i] = True
+            dfs(n+1, i+1)
+            visited[i] = False
 
-N = int(input())
-arr = [list(map(int,input().split())) for _ in range(N)]
-
-M = N // 2
-ans = 100*M*M
-
-dfs(0, [], [])
-print(ans)
+T = int(input())
+arr = [list(map(int,input().split()))for _ in range(T)]
+visited = [0] * (T+1)
+result = 1e9
+dfs(0,0)
+print(result)
