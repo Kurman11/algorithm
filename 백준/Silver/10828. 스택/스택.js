77ae769bc5c7eb6ae9c85d5c4b0e892cf77c7f10
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2024-09-10 15:21:00 +0900

    [Silver IV] Title: 스택, Time: 484 ms, Memory: 17020 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.js
+++ 백준/Silver/10828. 스택/스택.js
@@ -2,28 +2,38 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const [n,...arr] = input;
 const arr1 =[]
 
-for (let i = 0; i<n; i++){
-  if (arr[i].length == 2){
-    arr1.push(arr[i][1]);
-  } else if(arr[i] == 'pop'){
-    if (arr1.length == 0){
-      console.log(-1);
-    } else {
-      console.log(arr1.pop());
-    }
-  } else if(arr[i] == 'size'){
-    console.log(arr1.length);
-  } else if(arr[i] == 'empty'){
-    if(arr1.length == 0){
-      console.log(1)
-    }else{
-      console.log(0)
-    }
-  } else if(arr[i] == 'top'){
-    if(arr1.length == 0){
-      console.log(-1)
-    } else{
-      console.log(arr1[arr1.length-1])
-    }
+for(let i = 0; i < n; i++){
+  switch(arr[i][0]){
+    case 'pop':
+      if (arr1.length != 0){
+        console.log(arr1.pop())
+      } else {
+        console.log(-1)
+      }
+        break;
+
+      case 'size':
+        console.log(arr1.length)
+        break;
+
+      case 'empty':
+        if (arr1.length != 0){
+          console.log(0)
+        } else {
+          console.log(1)
+        }
+        break;
+
+      case 'top':
+        if (arr1.length != 0){
+          console.log(arr1[arr1.length - 1])
+        } else{
+          console.log(-1)
+        }
+        break;
+
+      default :
+        arr1.push(arr[i][1])
+        break;
   }
 }
