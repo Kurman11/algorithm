3ed19336d1ed58c8259dca686653dd831510d9e7
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2024-11-11 14:32:56 +0900

    [D2] Title: 간단한 소인수분해, Time: 120 ms, Memory: 44,780 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,16 +1,26 @@
-def simple(x):
-    d = 2
-    while d <= x:
-        if x % d == 0:
-            a.append(d)
-            x = x / d
-        else:
-            d = d + 1
+T = int(input())
 
-
-num = int(input())
-
-for i in range(1,num+1):
-    a = []
-    simple(int(input()))
-    print(f'#{i} {a.count(2)} {a.count(3)} {a.count(5)} {a.count(7)} {a.count(11)}')
+for i in range(1, T+1):
+    N = int(input())
+    a,b,c,d,e = 0,0,0,0,0
+    while 1:
+        if N % 2 == 0:
+            a += 1
+            N = N // 2
+        elif N % 3 == 0:
+            b += 1
+            N = N // 3
+        elif N % 5 == 0:
+            c += 1
+            N = N // 5
+        elif N % 7 == 0:
+            d += 1
+            N = N // 7
+        elif N % 11 == 0:
+            e += 1
+            N = N // 11
+        
+        if N <= 1:
+            break
+    
+    print(f'#{i} {a} {b} {c} {d} {e}')
