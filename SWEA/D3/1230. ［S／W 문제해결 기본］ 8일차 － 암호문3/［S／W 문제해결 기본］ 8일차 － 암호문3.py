6a6979f9fbdb5df8cdfc745b49a42442f0c1d891
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2024-11-16 16:36:33 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 259 ms, Memory: 51,368 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.py
@@ -0,0 +1,26 @@
+for i in range(1, 11):
+    T = int(input())
+    arr = list(map(int,input().split()))
+    code_num = int(input())
+    code = list(map(str,input().split()))
+    code_lst = [[] for _ in range(code_num)]
+    check = -1
+
+    for k in code:
+        if k == 'I' or k == 'D' or k == 'A':
+            check += 1
+        code_lst[check].append(k)
+
+    for j in code_lst:
+        if j[0] == 'I':
+            for k in range(int(j[2]),0,-1):
+                arr.insert(int(j[1]),int(j[k+2]))
+        elif j[0] == 'D':
+            for k in range(int(j[2])):
+                arr.pop(int(j[1]))
+
+        elif j[0] == 'A':
+            for k in range(int(j[1])):
+                arr.append(int(j[k+2]))
+
+    print(f'#{i} {" ".join(map(str,arr[0:10]))}')
