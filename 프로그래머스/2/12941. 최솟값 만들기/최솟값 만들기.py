9646834c8632208f523663b038494f7b87adc32c
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2025-04-01 18:33:22 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.43 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,8 +1,7 @@
 def solution(A,B):
-    answer = 0
     A.sort()
-    B.sort(reverse=True)
-    
+    B.sort(reverse = True)
+    answer = 0
     for i in range(len(A)):
         answer += A[i] * B[i]
 
