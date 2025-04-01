443b789badb4581c7868a380085e54a9baac9a7a
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2025-04-01 14:02:23 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,13 +1,15 @@
-N,K = map(int,input().split())
-arr = [i for i in range(1,N+1)]    # 맨 처음에 원에 앉아있는 사람들
+n,m = list(map(int, input().split()))
+arr = []
+rst = []
+cnt = 0
 
-answer = []   # 제거된 사람들을 넣을 배열
-num = 0  # 제거될 사람의 인덱스 번호
+for i in range(1, n+1):
+    arr.append(i)
 
-for t in range(N):
-    num += K-1  
-    if num >= len(arr):   # 한바퀴를 돌고 그다음으로 돌아올때를 대비해 값을 나머지로 바꿈  
-        num = num%len(arr)
- 
-    answer.append(str(arr.pop(num)))
-print("<",", ".join(answer)[:],">", sep='')
+for i in range(n):
+    cnt += m -1
+    if cnt >= len(arr):
+        cnt = cnt % len(arr)
+    rst.append(str(arr.pop(cnt)))
+
+print('<',", ".join(rst),">",sep='')
