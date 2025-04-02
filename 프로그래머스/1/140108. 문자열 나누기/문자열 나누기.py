488d76477ec69d66ebc3f273254659c8762c0764
Kurman11 <121422207+Kurman11@users.noreply.github.com>
2025-04-03 00:35:43 +0900

    [level 1] Title: 문자열 나누기, Time: 0.87 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,21 +1,13 @@
 def solution(s):
     answer = 0
-    cnt = [0, 0]  # [x의 개수, x가 아닌 글자들의 갯수]
-    cur = 0
-    while True:
-        flag = False
-        for i in range(cur, len(s)):
-            if cnt[0] == cnt[1] and cnt[0] != 0:
-                answer+=1
-                cnt = [0,0]
-                cur = i
-                break
-            if s[cur] != s[i]:
-                cnt[1] += 1
-            else:
-                cnt[0] += 1
-            if i == len(s)-1:
-                flag = True
-        if flag:
-            break
-    return answer+1
+    sav1=0
+    sav2=0
+    for i in s:
+        if sav1==sav2:
+            answer+=1
+            a=i
+        if i==a:
+            sav1+=1
+        else:
+            sav2+=1
+    return answer
